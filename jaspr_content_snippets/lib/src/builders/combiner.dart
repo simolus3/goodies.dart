4ab58ea6d3a14e262f6d9b6a999d1ecbf746e097
Simon Binder <oss@simonbinder.eu>
2025-10-03 14:50:21 +0200

    Disable Dart format

--- jaspr_content_snippets/lib/src/builders/combiner.dart
+++ jaspr_content_snippets/lib/src/builders/combiner.dart
@@ -1,10 +1,7 @@
 import 'dart:convert';
 
 import 'package:build/build.dart';
-import 'package:dart_style/dart_style.dart';
 import 'package:glob/glob.dart';
-import 'package:package_config/package_config_types.dart';
-import 'package:pub_semver/pub_semver.dart';
 
 import '../highlighted_excerpt.dart';
 
@@ -27,6 +24,7 @@ final class Combiner extends Builder {
   @override
   Future<void> build(BuildStep step) async {
     final buffer = StringBuffer('''
+// dart format off
 final generatedSnippets = {
 ''');
     var hadExcerpt = false;
@@ -49,28 +47,6 @@ final generatedSnippets = {
     buffer.writeln('};');
 
     if (hadExcerpt) {
-      var result = buffer.toString();
-
-      try {
-        final config = await step.packageConfig;
-        final version = config.packages.singleWhere(
-          (e) => e.name == step.inputId.package,
-        );
-
-        DartFormatter(
-          languageVersion: switch (version.languageVersion) {
-            LanguageVersion(:final major, :final minor) => Version(
-              major,
-              minor,
-              0,
-            ),
-            null => Version(3, 6, 0),
-          },
-        ).format(result);
-      } catch (e, s) {
-        log.warning('Could not format source', e, s);
-      }
-
       await step.writeAsString(step.allowedOutputs.single, buffer.toString());
     }
   }
